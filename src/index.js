5b0a28749c4e2f1a030a30c44d52ff8ddc13b2c2
Jorge Sarabia <jsarabiar98@gmail.com>
2022-04-15 18:54:06 -0500

    Números e Inmutabilidad

--- src/index.js
+++ src/index.js
@@ -4,18 +4,10 @@ const app = document.getElementById("app");
 app.innerHTML = "<h1>JavaScript Basics</h1>";
 
 // ----
-const result = Number('55px');
-
-// isNaN Retorna un valor true o false según el argumento enviado 
-console.log(isNaN(result));
-console.log(isNaN('I am a String!'));
-
-// El método NaN de Number retorna un boolean según el argumento enviado
-console.log(Number.NaN);
-console.log(Number.isNaN(result));
-console.log(Number.isNaN('I am another String!'));
-console.log(Number.isNaN(Number.NaN));
-
-// Verifica cuando un Numero es de tipo Integer
-console.log(Number.isInteger(66));
+const immutableNumber = 99;
+// Solo se almacena el valor de immutableNumber, mas no el tipo
+let referencedNumber = immutableNumber;
+referencedNumber = 44;
+console.log(immutableNumber);
+console.log(referencedNumber);
 
