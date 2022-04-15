c65ab1ae3220f3fa4b905405baef639228cd2143
Jorge Sarabia <jsarabiar98@gmail.com>
2022-04-15 18:49:59 -0500

    Comprendiendo NaN (Not a Number)

--- src/index.js
+++ src/index.js
@@ -4,19 +4,18 @@ const app = document.getElementById("app");
 app.innerHTML = "<h1>JavaScript Basics</h1>";
 
 // ----
-// Convierte un String en un Integer con base 10
-console.log(parseInt("55px", 10));
-// Si se escogiera base 2, se hace referencia a un num. binario
-console.log(parseInt("0101", 2));
+const result = Number('55px');
 
-// Convertir un String en Float
-console.log(parseFloat("55.9999px") * 10);
+// isNaN Retorna un valor true o false según el argumento enviado 
+console.log(isNaN(result));
+console.log(isNaN('I am a String!'));
 
-// Retorna un valor de 55 elevado a la 10a. potencia
-console.log(Number("55e10"));
-// Imprime el valor enviado al constructor
-console.log(Number("55.9999"));
-// Convierte un String a un número y hace la suma mediante el operador + +
-console.log(9 + +"99.5555");
-// Si solo se usa un operador, se mantiene el tipo String y concatena
-console.log(9 + "99.5555");
+// El método NaN de Number retorna un boolean según el argumento enviado
+console.log(Number.NaN);
+console.log(Number.isNaN(result));
+console.log(Number.isNaN('I am another String!'));
+console.log(Number.isNaN(Number.NaN));
+
+// Verifica cuando un Numero es de tipo Integer
+console.log(Number.isInteger(66));
+
