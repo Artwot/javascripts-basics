762b4ede2e35d54c469292e3e57687d00b9cc9b1
Jorge Sarabia <jsarabiar98@gmail.com>
2022-04-15 18:41:31 -0500

    Convirtiendo Strings a Numbers

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ const app = document.getElementById("app");
 app.innerHTML = "<h1>JavaScript Basics</h1>";
 
 // ----
+// Convierte un String en un Integer con base 10
+console.log(parseInt("55px", 10));
+// Si se escogiera base 2, se hace referencia a un num. binario
+console.log(parseInt("0101", 2));
 
-console.log(0.1 + 0.2);
+// Convertir un String en Float
+console.log(parseFloat("55.9999px") * 10);
 
-const price = 9.33;
-const quantity = 3;
-console.log(price * quantity);
-
-// Para redondear un numero
-const anotherPrice = 9.33 * 100;
-const anotherQuantity = 3;
-console.log((anotherPrice * anotherQuantity) / 100);
-
+// Retorna un valor de 55 elevado a la 10a. potencia
+console.log(Number("55e10"));
+// Imprime el valor enviado al constructor
+console.log(Number("55.9999"));
+// Convierte un String a un número y hace la suma mediante el operador + +
+console.log(9 + +"99.5555");
+// Si solo se usa un operador, se mantiene el tipo String y concatena
+console.log(9 + "99.5555");
