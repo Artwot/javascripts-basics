d0717b1e282bcc51f16685d15a7efdfda6dc4580
Jorge Sarabia <jsarabiar98@gmail.com>
2022-04-15 18:10:42 -0500

    Número Literal, Función & Constructor

--- src/index.js
+++ src/index.js
@@ -4,25 +4,16 @@ const app = document.getElementById("app");
 app.innerHTML = "<h1>JavaScript Basics</h1>";
 
 // ----
-const number = 1000;
-let result;
+// literal
+const literalNumber = 99;
+console.log(literalNumber);
 
-switch (number) {
-  case 1: {
-    const text = 'One';
-    result = text;
-    break;
-  }
-  case 99: {
-    const text = 'Ninety-Nine';
-    result = text;
-    break;
-  }
-  case 1000:
-    result = 'One Thousand';
-    break;
-  default:
-    result = 'No Match';
-}
+// sintaxis de función
+// realiza una conversión dentro de un contexto de no-constructor
+console.log(Number(55));
+console.log(Number('33'));
+console.log(Number('44px'));  // retorna 'NaN'
 
-console.log(result);
+// sintaxis de constructor, crea un objeto contenedor
+// avoid using it
+console.log(new Number('44px'));
