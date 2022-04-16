7256d093a491ec5bf25520bba6490559224e341e
Jorge Sarabia <jsarabiar98@gmail.com>
2022-04-15 19:34:10 -0500

    Sintaxis String Literal, Function & Constructor

---
+++ src/Strings/app.js
@@ -0,0 +1,12 @@
+console.log('Pizza "Hello!"');
+
+const pizza = "Pepperoni";
+console.log("Pizza is: " + pizza + "!");
+console.log(`Pizza is: ${pizza}!`);
+
+console.log(String(55 + 11), 55 + "11");
+console.log(String({ name: "Pepperoni" }));
+console.log(String([1, 2, 3, 4]));
+
+// Almacena cada caracter en una posición
+console.log(new String(55));
