b84336e562c776f4bcd25419a8c2aed7b1d7edaf
Jorge Sarabia <jsarabiar98@gmail.com>
2022-04-15 19:14:42 -0500

    Métodos del objeto Number

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ const app = document.getElementById("app");
 app.innerHTML = "<h1>JavaScript Basics</h1>";
 
 // ----
-// Verifica el tipo de dato de un valor o variable
-console.log(typeof 99.66);
-// Verifica si el valor es instancia de una clase
-console.log(99 instanceof Number);
-console.log(Number("99") instanceof Number); // false
-// Se crea una nueva instancia de Number, por lo cual retorna true
-console.log(new Number("99") instanceof Number);
-console.log(Object.prototype.toString.call(99).slice(8, -1) === "Number");
-
+// Imprime el prototipo del objeto
+console.log(Number.prototype);
+// Toma los métodos pertenecientes al objeto Number
+console.log(parseFloat((99.12345678).toFixed(4)));
+console.log((99.12345678).toPrecision(5));
+console.log(new Number(99).valueOf());
